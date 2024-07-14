418037eaeda4920294842971524e6c149761f0a4
sQurban <45127669+sQurban40@users.noreply.github.com>
2024-07-15 01:12:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import sklearn
 import pickle
 import matplotlib.pyplot as plt
 def plot_data(x,y,clr):
-    st.header("Data Visualization")
     fig, ax = plt.subplots()
     plt.scatter(x, y, color=clr,label=y.name)
     plt.title(f'Age vs {y.name}')
@@ -38,6 +37,7 @@ def main():
         st.write(protein3.head())
     if st.button("Analyze Data"): 
         if Protien1_file and Protien2_file and Protien3_file:
+            st.header("Data Visualization")
             protein1.rename(columns={'Methylation (%)':'Methylation_prot1'},inplace=True)
             protein1.Age=protein1.Age.round(3)
             plot_data(protein1['Age'],protein1['Methylation_prot1'],"lightblue")
@@ -52,9 +52,11 @@ def main():
             
             all_data=pd.concat([protein1, protein2,protein3])
             all_data.sort_values(by='Age',inplace=True)
-            st.markdown("Combining 3 protien Data", unsafe_allow_html = True)
+            st.markdown("Combining 3 protein Data", unsafe_allow_html = True)
             st.write(all_data.head())
             #plot_data(protein1['Age'],protein1['Methylation_prot1'],"lightblue")
+        else:
+            st.warning("Please upload a all three Protein data files.")
 
 
 
